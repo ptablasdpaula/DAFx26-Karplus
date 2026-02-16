a3a3fac3c44513bd81e37de3b7bd05666ad77461
Pablo Tablas <acw794@qmul.ac.uk>
2026-02-16 15:49:31 +0000

    Finalises Fig. 1

--- synths/utils.py
+++ synths/utils.py
@@ -2,17 +2,19 @@ import librosa, torchcrepe, numpy as np, torch
 from synths.constants import DEFAULT_FS, DEFAULT_CREPE_HOP_LENGTH, DEFAULT_ONSET_HOP_LENGTH, DEFAULT_ONSET_PAD_DURATION
 
 def detect_onsets(audio_np, sr=DEFAULT_FS, hop_length=DEFAULT_ONSET_HOP_LENGTH, pad_duration=DEFAULT_ONSET_PAD_DURATION):
-    """Detect onsets with padding."""
     pad_samples = int(pad_duration * sr)
     audio_padded = np.pad(audio_np, (pad_samples, 0), mode='constant', constant_values=0)
 
     onset_frames = librosa.onset.onset_detect(
-        y=audio_padded, sr=sr, hop_length=hop_length,
-        backtrack=True, units='frames'
+        y=audio_padded,
+        sr=sr,
+        hop_length=hop_length,
+        backtrack=True,
+        units='frames',
+        onset_envelope=None,  # Spectral flux
     )
 
     duration = len(audio_np) / sr
-
     onset_times = librosa.frames_to_time(onset_frames, sr=sr, hop_length=hop_length) - pad_duration
     onset_times = onset_times[onset_times >= -0.05]
     onset_times = np.clip(onset_times, 0, duration)
@@ -22,8 +24,8 @@ def detect_onsets(audio_np, sr=DEFAULT_FS, hop_length=DEFAULT_ONSET_HOP_LENGTH, 
 def detect_f0(audio_tensor, sr=DEFAULT_FS, hop_length=DEFAULT_CREPE_HOP_LENGTH):
     f0, confidence = torchcrepe.predict(
         audio_tensor.float(), sr, hop_length=hop_length,
-        fmin=40, fmax=2000, model='full', batch_size=2048,
-        device='cpu', return_periodicity=True
+        fmin=40, fmax=2000, model='full', batch_size=256,
+        device='cpu', return_periodicity=True,
     )
 
     f0_np = f0.squeeze().cpu().numpy()
