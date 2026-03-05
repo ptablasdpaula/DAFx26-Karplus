4a2da7037bbb10c8eda0e11240f55ef3a7ca91df
Pablo Tablas <acw794@qmul.ac.uk>
2026-03-05 11:25:54 +0000

    Computes Precision, Recall and F1 (onsets)

--- src/metrics.py
+++ src/metrics.py
@@ -3,15 +3,40 @@ import librosa
 
 from src.synths.constants import DEFAULT_FS
 from dtw import dtw
+import mir_eval
 
-# @TODO: Precision, Recall, F1 on Onsets (Synthetic Only)
+#==================================================================================================
+# PRECISION, RECALL & F1 (ONSETS)
+#==================================================================================================
+def from_gain_frames_to_onsets_seconds(
+        frames: np.ndarray,
+        duration_s: float,
+) -> np.ndarray:
+    num_frames = len(frames)
+    hop_seconds = duration_s / num_frames
+    onsets = np.flatnonzero(frames > 0)
+    return onsets * hop_seconds
 
+def compute_onset_precision_recall(pred_s, target_s, tolerance_s = 0.025):
+    scores = mir_eval.onset.evaluate(
+        target_s,
+        pred_s,
+        window=tolerance_s
+    )
+    return scores["Precision"], scores['Recall'], scores['F-measure']
+
+#==================================================================================================
+# CENTS
+#==================================================================================================
 def compute_mean_cents_distance(pred_f0: np.ndarray, target_f0: np.ndarray) -> float:
     high_f0 = np.maximum(pred_f0, target_f0)
     low_f0 = np.minimum(pred_f0, target_f0)
     cents_distance = 1200 * np.log2(high_f0 / low_f0)
     return np.mean(cents_distance)
 
+#==================================================================================================
+# MFCC
+#==================================================================================================
 def compute_mfcc(x: np.ndarray, sample_rate: int = DEFAULT_FS) -> np.ndarray:
     window_length = int(0.05 * sample_rate)
     hop_length = int(0.01 * sample_rate)
@@ -40,6 +65,9 @@ def compute_wmfcc(target: np.ndarray, pred: np.ndarray, sample_rate: int = DEFAU
     dist = dtw(target_mfcc.T, pred_mfcc.T, dist_method=l1, distance_only=True)
     return dist.normalizedDistance
 
+#==================================================================================================
+# RMS
+#==================================================================================================
 def compute_rms(target: np.ndarray, pred: np.ndarray, sample_rate: int = DEFAULT_FS) -> float:
     win_length = int(0.05 * sample_rate)
     hop_length = int(0.025 * sample_rate)
